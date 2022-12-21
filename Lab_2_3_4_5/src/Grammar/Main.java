f63a7f636f3cb09dcf5c91f32794d902c84a2221
Quackdratic <39701964+Quackdratic@users.noreply.github.com>
2022-12-21 02:45:41 +0200

    g2 example

--- Lab_2_3_4_5/src/Grammar/Main.java
+++ Lab_2_3_4_5/src/Grammar/Main.java
@@ -2,7 +2,7 @@ package Grammar;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        var grammar = new Grammar("src\\g1.txt");
+        var grammar = new Grammar("src\\g2.txt");
         //  var grammar = new Grammar("src\\g1.txt");
         var parser = new ParserLR0(grammar);
         parser.printGotoTable();
